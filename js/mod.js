7a82213ffefe59a75dc3a46bd68cd8ad93b7cbea
nova250p <159670537+nova250p@users.noreply.github.com>
2024-10-06 21:32:31 -0500

    g

--- js/mod.js
+++ js/mod.js
@@ -7,7 +7,7 @@ let modInfo = {
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (1e50), // Used for hard resets and new players
+	initialStartPoints: new Decimal (10), // Used for hard resets and new players
 	offlineLimit: 24,  // In hours
 }
 
