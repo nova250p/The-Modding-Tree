35904dcfb33a4eb0c41ffa8129f0a052d90c112a
nova250p <159670537+nova250p@users.noreply.github.com>
2024-10-06 20:51:35 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The infinity Tree",
+	id: "criticals",
+	author: "void",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
