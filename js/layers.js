a16f97832dc4a287ae0c50c76c7cc565878c44da
nova250p <159670537+nova250p@users.noreply.github.com>
2024-10-06 21:48:01 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -25,4 +25,8 @@ addLayer("p", {
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
-})
+    },
+    upgrades, {
+
+    },
+)
